93270cfdd3a8455f4422b8fbc29c6d83f1750f54
Jake Lazaroff <jake@parsec.tv>
2017-07-20 14:32:51 -0500

    Fetch images dynamically

--- nginx/html/js/index.js
+++ nginx/html/js/index.js
@@ -1,7 +1,49 @@
 ((root, dom) => {
 
-  const SCROLL_THRESHOLD = 10;
+  // IMAGES
+  const imageTemplate = image => `
+    <div class="screenshot">
+      <img class="screenshot-image" src="${ image }">
+    </div>
+  `.trim();
 
+  const createElement = template => model => {
+    const wrapper = dom.createElement('template');
+    wrapper.innerHTML = template(model);
+    return dom.importNode(wrapper.content, true);
+  };
+
+  const renderImage = createElement(imageTemplate);
+
+  // FETCH
+  const elementIsVisible = el => {
+    const rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+  }
+
+  const gallery = document.querySelector('.js-gallery');
+  const fetchImages = () => (
+    fetch('/')
+      .then(response => response.json())
+      .then(response => {
+        const fragment = document.createDocumentFragment();
+        response.images
+          .map(renderImage)
+          .forEach(fragment.appendChild.bind(fragment));
+
+        gallery.appendChild(fragment);
+
+        [].slice.call(gallery.childNodes, -3).forEach(image => {
+          image.classList.add('next-screenshot');
+          image.classList.add('js-next-screenshot');
+        });
+      })
+  );
+
+  fetchImages();
+
+  // INFINITE SCROLL
+  const SCROLL_THRESHOLD = 10;
   window.addEventListener('scroll', () => {
     const position = document.body.scrollTop + window.innerHeight;
     const height = document.body.clientHeight;
@@ -23,8 +65,8 @@
     }
   });
 
+  // UPLOAD
   const uploader = dom.querySelector('.uploader');
-
   uploader.addEventListener('change', () => {
     const data = new FormData();
     data.append('image', uploader.files[0]);
