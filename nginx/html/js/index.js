f3e764b9d1c28bab60f982b6a9500cb2c403a332
Jake Lazaroff <jake@parsec.tv>
2017-07-21 17:33:39 -0500

    Add social buttons

--- nginx/html/js/index.js
+++ nginx/html/js/index.js
@@ -2,9 +2,27 @@
 
   // IMAGES
   const imageTemplate = image => `
-    <a class="screenshot" href="/?${ image }">
+    <div class="screenshot" href="/?${ image }">
       <img class="screenshot-image" src="/images/${ image }">
-    </a>
+      <div class="screenshot-share">
+        <a class="screenshot-share-link" href="#">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+            <g transform="translate(0, 0)">
+              <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788
+            c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2
+            V2C24,0.895,23.105,0,22,0z"></path>
+            </g>
+          </svg>
+        </a>
+        <a class="screenshot-share-link" href="#">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+            <g transform="translate(0, 0)">
+              <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6C22.5,6.4,23.3,5.5,24,4.6z"></path>
+            </g>
+          </svg>
+        </a>
+      </div>
+    </div>
   `.trim();
 
   const heroTemplate = image => `
@@ -94,49 +112,49 @@
   };
 
   // UPLOAD
-  const overlay = dom.querySelector('.js-overlay');
-  overlay.addEventListener('click', () => {
-    overlay.classList.remove('visible');
-  });
+  // const overlay = dom.querySelector('.js-overlay');
+  // overlay.addEventListener('click', () => {
+  //   overlay.classList.remove('visible');
+  // });
 
-  const modal = dom.querySelector('.js-modal');
-  modal.addEventListener('click', event => {
-    event.stopPropagation();
-  });
+  // const modal = dom.querySelector('.js-modal');
+  // modal.addEventListener('click', event => {
+  //   event.stopPropagation();
+  // });
 
-  const uploadToggle = dom.querySelector('.js-upload-toggle');
-  uploadToggle.addEventListener('click', () => {
-    overlay.classList.toggle('visible');
-  });
+  // const uploadToggle = dom.querySelector('.js-upload-toggle');
+  // uploadToggle.addEventListener('click', () => {
+  //   overlay.classList.toggle('visible');
+  // });
 
-  const uploadInput = dom.querySelector('.js-upload-input');
+  // const uploadInput = dom.querySelector('.js-upload-input');
 
-  const uploadSubmit = dom.querySelector('.js-upload-submit');
-  uploadSubmit.addEventListener('click', () => {
-    const data = new FormData();
-    data.append('image', uploadInput.files[0]);
+  // const uploadSubmit = dom.querySelector('.js-upload-submit');
+  // uploadSubmit.addEventListener('click', () => {
+  //   const data = new FormData();
+  //   data.append('image', uploadInput.files[0]);
 
-    fetch('/images', {
-      method: 'POST',
-      body: data,
-    })
-      .then(response => response.json())
-      .then(response => {
-        gallery.prepend(renderImage(response.path));
-        overlay.classList.remove('visible');
-      });
-  });
+  //   fetch('/images', {
+  //     method: 'POST',
+  //     body: data,
+  //   })
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       gallery.prepend(renderImage(response.path));
+  //       overlay.classList.remove('visible');
+  //     });
+  // });
 
   // BOOTSTRAP
-  if (root.location.search) {
-    const hero = dom.querySelector('.js-hero');
-    hero.appendChild(renderHero(root.location.search.slice(1)));
-    document.querySelectorAll('.js-hero-page').forEach(node => node.classList.add('visible'));
-  } else {
+  // if (root.location.search) {
+  //   const hero = dom.querySelector('.js-hero');
+  //   hero.appendChild(renderHero(root.location.search.slice(1)));
+  //   document.querySelectorAll('.js-hero-page').forEach(node => node.classList.add('visible'));
+  // } else {
     fetchImages().then(appendImages);
     root.addEventListener('scroll', onScroll);
-    document.querySelectorAll('.js-gallery-page').forEach(node => node.classList.add('visible'));
-  }
+    // document.querySelectorAll('.js-gallery-page').forEach(node => node.classList.add('visible'));
+  // }
 
 
 })(window, document);
